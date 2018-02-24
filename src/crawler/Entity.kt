ca06080268ef3dd50b4ea67205c1b2d45d33b1aa
djc <727809864@qq.com>
2018-02-24 16:35:15 +0800

    first commit

---
+++ src/crawler/Entity.kt
@@ -0,0 +1,3 @@
+package crawler
+
+data class Emoji(var name: String? = null, var href: String? = null)
---
+++ src/crawler/Main.kt
@@ -0,0 +1,74 @@
+package crawler
+
+import org.jsoup.Jsoup
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+
+val list = ArrayList<Emoji>()
+lateinit var string: StringBuilder
+
+fun main(args: Array<String>) {
+    getAllEmojis()
+    string = StringBuilder("\"")
+    list.forEach { emoji ->
+        getDetailEmoji(emoji);
+    }
+}
+
+//遍历所有1.0的emoji
+fun getAllEmojis() {
+    val mainUrlDoc = Jsoup.connect("https://emojipedia.org/emoji-1.0/").get()
+    val ulElements = mainUrlDoc?.getElementsByTag("ul")
+    val emojiListElement = ulElements?.get(2)
+    val emojiElements = emojiListElement?.getElementsByTag("li")
+    list.clear()
+    emojiElements?.forEach { e ->
+        val href = e?.getElementsByTag("a")?.first()?.attr("abs:href")
+        val text = e?.getElementsByClass("emoji")?.text()
+        list.add(Emoji(text, href))
+    }
+}
+
+//获取表情详情页里面的内容
+fun getDetailEmoji(emoji: Emoji?) {
+    val specificEmoji = Jsoup.connect(emoji?.href)?.get()
+    val apple = specificEmoji?.select("html body div.container div.content article section.vendor-list ul li div.vendor-container.vendor-rollout-target div.vendor-image img")
+            ?.first()
+    val lowerCase = StringUtil.string2Unicode(emoji?.name)?.toLowerCase()
+    val url = apple?.attr("src")
+    val name = lowerCase?.replace('\\', '_')
+    saveImage(name, url)
+    saveName(lowerCase)
+}
+
+//保存图片
+fun saveImage(name: String?, imageUrl: String?) {
+    val file = File("emoji/$name.png")
+    if (file.exists()) {
+        file.delete()
+    } else if (!file.parentFile.exists()) {
+        file.parentFile.mkdirs()
+    }
+    val connect = URL(imageUrl).openConnection() as? HttpURLConnection
+    val inputStream = connect?.inputStream
+    FileUtil.save(inputStream, file)
+    try {
+        inputStream?.close()
+    } catch (e: Throwable) {
+        e.printStackTrace()
+    }
+    println("down $imageUrl to $file already")
+}
+
+//保存名称到文本文件
+fun saveName(name: String?) {
+    string.append(name).append('"').append(',').append('"')
+    val file = File("emoji/emojis.txt")
+    file.deleteOnExit()
+    FileUtil.save(string.toString().byteInputStream(), file)
+}
