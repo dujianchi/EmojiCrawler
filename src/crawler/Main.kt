9cb71a33d959e56a979b3baabd0cff922ec9bcb3
djc <727809864@qq.com>
2018-03-01 15:32:47 +0800

    完善；自动输出匹配当前爬取到的emoji表情的Unicode的正则表达式

--- src/crawler/Main.kt
+++ src/crawler/Main.kt
@@ -7,8 +7,8 @@ import java.io.InputStream
 import java.lang.StringBuilder
 import java.net.HttpURLConnection
 import java.net.URL
-import java.util.*
 import java.util.concurrent.Executors
+import kotlin.collections.ArrayList
 
 // ---------------------------------------------
 val BUFFER_SIZE = 1024;
@@ -20,16 +20,19 @@ val GOOGLE_STYLE = 1;
 val STYLE = GOOGLE_STYLE
 //线程数
 val THREADS = 16
+//需要停止的那个表情，结束标志
+val STOP_ONE = "\uD83E\uDD13"
 // ---------------------------------------------
 
 val excutor = Executors.newFixedThreadPool(THREADS)
-val list = ArrayList<Emoji>()
-lateinit var string: StringBuilder
+val emoji_list = ArrayList<Emoji>()
+val name_list = ArrayList<String>()
+lateinit var tmp_name: StringBuilder
 
 fun main(args: Array<String>) {
     getAllEmojis()
-    string = StringBuilder("\"")
-    list.forEachIndexed { index, emoji ->
+    tmp_name = StringBuilder("\"")
+    emoji_list.forEachIndexed { index, emoji ->
         excutor.execute { getDetailEmoji(emoji); }
     }
 }
@@ -40,14 +43,15 @@ fun getAllEmojis() {
     val ulElements = mainUrlDoc?.getElementsByTag("ul")
     val emojiListElement = ulElements?.get(2)
     val emojiElements = emojiListElement?.getElementsByTag("li")
-    list.clear()
+    emoji_list.clear()
+    name_list.clear()
     run breaking@ {
         //这是设置到某个值时可以直接break的
         emojiElements?.forEach { e ->
             val href = e?.getElementsByTag("a")?.first()?.attr("abs:href")
             val text = e?.getElementsByClass("emoji")?.text()
-            list.add(Emoji(text, href))
-            //if ("\uD83E\uDD13".equals(text)) return@breaking//这是设置到某个值时可以直接break的
+            emoji_list.add(Emoji(text, href))
+            if (STOP_ONE.equals(text)) return@breaking//这是设置到某个值时可以直接break的
         }
     }
 }
@@ -62,6 +66,12 @@ fun getDetailEmoji(emoji: Emoji?) {
     val name = lowerCase?.replace('\\', '_')
     saveImage(name, url)
     saveName(lowerCase)
+    if (lowerCase != null) name_list.add(lowerCase)
+    if (name_list.size == emoji_list.size) {
+        val generateRegex = generateRegex(name_list.sorted())
+        val file = File("emoji/regex.txt")
+        streamToFile(generateRegex.byteInputStream(), file)
+    }
 }
 
 //保存图片
@@ -85,10 +95,10 @@ fun saveImage(name: String?, imageUrl: String?) {
 
 //保存名称到文本文件
 fun saveName(name: String?) {
-    string.append(name).append('"').append(',').append('"')
+    tmp_name.append(name).append('"').append(',').append('"')
     val file = File("emoji/emojis.txt")
     file.deleteOnExit()
-    val byteInputStream = string.toString().byteInputStream()
+    val byteInputStream = tmp_name.toString().byteInputStream()
     streamToFile(byteInputStream, file)
 }
 
@@ -105,4 +115,39 @@ fun streamToFile(inputStream: InputStream?, file: File?) {
     } catch (e: Throwable) {
         e.printStackTrace()
     }
+}
+
+fun generateRegex(emojis: List<String>) :String{
+    val map = linkedMapOf<String, Int>()
+    emojis.forEach {
+        var c = 0
+        it.toCharArray().forEach { c += it.toInt() }
+        map.put(it, c)
+    }
+    val regex = StringBuilder().append('[')
+    var index = 0
+    var lastCode = 0
+    map.forEach { name, code ->
+        val append = name
+        if (index++ == 0) {
+            regex.append(append)
+        } else {
+            if (code - lastCode == 1) {
+                val lastIndexOf = regex.lastIndexOf('-')
+                if (lastIndexOf == regex.length - 1) {
+                    regex.append(append)
+                } else if (lastIndexOf == -1) {
+                    regex.append('-').append(append)
+                } else {
+                    regex.replace(lastIndexOf + 1, regex.length, "$append")
+                }
+            } else {
+                regex.append('|').append(append)
+            }
+        }
+        lastCode = code
+    }
+    regex.append(']')
+    println(regex)
+    return regex.toString()
 }
